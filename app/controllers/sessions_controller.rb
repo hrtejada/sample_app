0d2a9cad50b4f2b8637c619f2e48ce6fc5d4aeda
hrtejada <hr.tejada121@gmail.com>
2016-01-29 11:16:47 -0700

    Finished update, user index and delete action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
       log_in user #log_in in sesion helper
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user #Uses session token to check if user is saved on browser
-      redirect_to user #redirect used for path to user_url(user)
+      #remember user #Uses session token to check if user is saved on browser
+      redirect_back_or user #redirect used for path to user_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination' #Flash.now is for messages to be displayed and removed after new request is sent
       render 'new'
